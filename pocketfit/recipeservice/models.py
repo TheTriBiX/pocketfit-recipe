01ded21e63bf5e8db7157f6ffae2c5ebaa9442a5
TheTriBiX <57715605+TheTriBiX@users.noreply.github.com>
2024-05-13 13:41:36 +0300

    added Ingredient module

--- pocketfit/recipeservice/models.py
+++ pocketfit/recipeservice/models.py
@@ -9,10 +9,34 @@ class Allergy(models.Model):
     name = models.CharField(null=False, max_length=100, unique=True)
     translations = models.JSONField(null=True)
 
+    def __str__(self):
+        return self.name
+
 
 class UserAllergy(models.Model):
     class Meta:
         db_table = 'user_allergy'
+        unique_together = ('user_id', 'allergy_id')
 
-    user_uuidv4 = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=100)
+    allergy_id = models.IntegerField()
+
+
+class Ingredients(models.Model):
+    class Meta:
+        db_table = 'Ingredients'
+
+    name = models.CharField(null=False, max_length=100, unique=True)
+    translations = models.JSONField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class IngredientsAllergy(models.Model):
+    class Meta:
+        db_table = 'IngredientsAllergy'
+        unique_together = ('ingredient_id', 'allergy_id')
+
+    ingredient_id = models.IntegerField()
     allergy_id = models.IntegerField()
--- pocketfit/recipeservice/services.py
+++ pocketfit/recipeservice/services.py
@@ -2,14 +2,14 @@ import grpc
 from google.protobuf import empty_pb2
 from recipe_proto import allergy_pb2
 from django_grpc_framework.services import Service
-from recipeservice.models import Allergy, UserAllergy
+from recipeservice.models import Allergy, UserAllergy, Ingredients, IngredientsAllergy
 from recipeservice.serializers import *
 from django.db import IntegrityError
 
 
 def find_translation(target, translations):
     for translate in translations:
-        if target in translate['language']:
+        if target in translate['languages']:
             return [translate]
 
 
@@ -103,5 +103,171 @@ class AllergyService(Service):
                 Allergy.objects.get(id=data['id']).delete()
             except Allergy.DoesNotExist:
                 response['error_code'] = 2
-        print(response)
+        return serializer.data_to_message(response)
+
+
+class UserAllergyService(Service):
+    def List(self, request, context):
+        serializer = UserAllergyListSerializer()
+        data = serializer.message_to_data(request)
+        response = {'error_code': 0}
+        if not data['user_id']:
+            return serializer.data_to_message(response)
+        allergies = UserAllergy.objects.filter(user_id=data['user_id'])
+        response['user_id'] = data['user_id']
+        response['allergies_id'] = [i.pk for i in allergies]
+        return serializer.data_to_message(response)
+
+    def Create(self, request, context):
+        serializer = UserAllergyCreateSerializer()
+        data = serializer.message_to_data(request)
+        response = {'error_code': 0, 'user_id': data['user_id'], 'allergy_id': data['allergy_id']}
+        if not (data['user_id'] and data['allergy_id']):
+            response['error_code'] = 1
+            return serializer.data_to_message(response)
+        try:
+            allergy_id = Allergy.objects.get(pk=data['allergy_id']).id
+            new_obj = UserAllergy(allergy_id=allergy_id, user_id=data['user_id']).save()
+        except Allergy.DoesNotExist:
+            response['error_code'] = 2
+        except IntegrityError:
+            allergy_id = Allergy.objects.get(pk=data['allergy_id']).id
+            response['error_code'] = 3
+        return serializer.data_to_message(response)
+
+    def Destroy(self, request, context):
+        serializer = UserAllergyDestroySerializer()
+        data = serializer.message_to_data(request)
+        response = {'error_code': 0}
+        if not (data['user_id'] and data['allergy_id']):
+            response['error_code'] = 1
+            return serializer.data_to_message(response)
+        try:
+            obj = UserAllergy.objects.get(user_id=data['user_id'], allergy_id=data['allergy_id']).delete()
+        except UserAllergy.DoesNotExist:
+            response['error_code'] = 2
+        return serializer.data_to_message(response)
+
+
+class IngredientService(Service):
+    def Create(self, request, context):
+        serializer = IngredientCreateSerializer()
+        data = serializer.message_to_data(message=request)
+        response = {'error_code': 0}
+        if not data['name'] or not data['translations']:
+            response['error_code'] = 1
+            return serializer.data_to_message(data)
+        try:
+            new_obj = Ingredients(name=data['name'], translations=data['translations'])
+            new_obj.save()
+            response['ingredient'] = {'id': new_obj.pk,
+                                      'name': new_obj.name,
+                                      'translations': new_obj.translations}
+        except IntegrityError:
+            existed_obj = Ingredients.objects.get(name=data['name'])
+            response['ingredient'] = {'id': existed_obj.pk,
+                                      'name': existed_obj.name,
+                                      'translations': existed_obj.translations}
+            response['error_code'] = 2
+
+        return serializer.data_to_message(response)
+
+    def List(self, request, context):
+        serializer = IngredientListSerializer()
+        data = serializer.message_to_data(request)
+        response = {'error_code': 0}
+        if not data:
+            response['ingredients'] = [{'id': ingredient.pk,
+                                        'name': ingredient.name,
+                                        'translations': ingredient.translations} for ingredient in
+                                       Ingredients.objects.all()]
+        elif 'language' in data:
+            response['ingredients'] = [{'id': ingredient.pk,
+                                        'name': ingredient.name,
+                                        'translations': find_translation(data['language'], ingredient.translations)}
+                                       for ingredient in Ingredients.objects.all()]
+        else:
+            response['error_code'] = 1
+        return serializer.data_to_message(response)
+
+    def Retrieve(self, request, context):
+        serializer = IngredientRetrieveSerializer()
+        data = serializer.message_to_data(request)
+        response = {'error_code': 0}
+        if data['ingredient_id'] != 0:
+            try:
+                ingredient = Ingredients.objects.get(pk=data['ingredient_id'])
+                translation = find_translation(data['language'],
+                                               ingredient.translations) if data['language'] else ingredient.translations
+                response['ingredient'] = {'id': ingredient.pk,
+                                          'name': ingredient.name,
+                                          'translations': translation}
+            except Ingredients.DoesNotExist:
+                response['error_code'] = 2
+        else:
+            response['error_code'] = 1
+        return serializer.data_to_message(response)
+
+    def Update(self, request, context):
+        serializer = IngredientUpdateSerializer()
+        data = serializer.message_to_data(request)
+        response = {'error_code': 0}
+        if data and data['ingredient']['id'] != 0:
+            try:
+                ingredient = Ingredients.objects.get(pk=data['ingredient']['id'])
+                ingredient.name = data['ingredient']['name']
+                ingredient.translations = data['ingredient']['translations']
+                response['ingredient'] = {'id': ingredient.pk,
+                                          'name': ingredient.name,
+                                          'translations': ingredient.translations}
+            except Ingredients.DoesNotExist:
+                response['error_code'] = 2
+        else:
+            response['ingredient'] = None
+            response['error_code'] = 1
+        return serializer.data_to_message(response)
+
+    def Destroy(self, request, context):
+        serializer = IngredientDestroySerializer()
+        data = serializer.message_to_data(request)
+        response = {'error_code': 0}
+        if not data['ingredient_id']:
+            response['error_code'] = 1
+        else:
+            try:
+                Ingredients.objects.get(id=data['ingredient_id']).delete()
+            except Ingredients.DoesNotExist:
+                response['error_code'] = 2
+        return serializer.data_to_message(response)
+
+    def AddAllergy(self, request, context):
+        serializer = IngredientAddAllergySerializer()
+        data = serializer.message_to_data(request)
+        response = {'error_code': 0, 'allergy_id': data['allergy_id'], 'ingredient_id': data['ingredient_id']}
+        if not (data['allergy_id'] and data['ingredient_id']):
+            response['error_code'] = 1
+            return serializer.data_to_message(response)
+        try:
+            ingredient = Ingredients.objects.get(pk=data['ingredient_id'])
+            allergy = Allergy.objects.get(pk=data['allergy_id'])
+            new_obj = IngredientsAllergy(allergy_id=allergy.pk, ingredient_id=ingredient.pk).save()
+        except Allergy.DoesNotExist:
+            response['error_code'] = 2
+        except Ingredients.DoesNotExist:
+            response['error_code'] = 3
+        except IntegrityError:
+            response['error_code'] = 4
+        return serializer.data_to_message(response)
+
+    def DestroyAllergy(self, request, context):
+        serializer = IngredientDestroyAllergySerializer()
+        data = serializer.message_to_data(request)
+        response = {'error_code': 0, 'allergy_id': data['allergy_id'], 'ingredient_id': data['ingredient_id']}
+        if not (data['allergy_id'] and data['ingredient_id']):
+            response['error_code'] = 1
+            return serializer.data_to_message(response)
+        try:
+            IngredientsAllergy.objects.get(allergy_id=data['allergy_id'], ingredient_id=data['ingredient_id']).delete()
+        except IngredientsAllergy.DoesNotExist:
+            response['error_code'] = 2
         return serializer.data_to_message(response)
