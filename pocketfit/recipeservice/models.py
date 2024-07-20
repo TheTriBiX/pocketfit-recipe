19045661ec666ac1ead0067a8207d90f4aea9f33
Илья Терешенко <ilya_tereshenko04@mail.ru>
2024-07-20 12:49:11 +0300

    Добавил модели и поле к существующим

--- pocketfit/recipeservice/models.py
+++ pocketfit/recipeservice/models.py
@@ -1,13 +1,35 @@
 from django.db import models
 
+class Ingredients(models.Model):
+
+    id = models.AutoField(primary_key=True, auto_created=True)
+    name = models.CharField(null=False, max_length=100, unique=True)
+    translations = models.JSONField(null=True)
+    Squirrels = models.IntegerField()
+    Fats = models.IntegerField()
+    Carbohydrates = models.IntegerField() 
+    PFC = models.IntegerField() # надо брать из рассчета на m(=100г?)
+    comment = models.TextField(max_length=2000)
+    same_ingridient = models.ForeignKey("self", related_name='related_ingridients', on_delete=models.CASCADE, null=True, blank=True)
+    image = models.CharField(max_length=255)
+    class Meta:
+        db_table = 'Ingredients'
+    def __str__(self):
+        return self.name
+
+
 
 # Create your models here.
 class Allergy(models.Model):
     class Meta:
         db_table = 'allergy'
 
-    name = models.CharField(null=False, max_length=100, unique=True)
-    translations = models.JSONField(null=True)
+    id = models.AutoField(primary_key=True, auto_created=True)
+    name = models.CharField(max_length=255)
+    translations = models.JSONField()
+    comment = models.TextField()
+    foods = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.name
@@ -22,17 +44,6 @@ class UserAllergy(models.Model):
     allergy_id = models.IntegerField()
 
 
-class Ingredients(models.Model):
-    class Meta:
-        db_table = 'Ingredients'
-
-    name = models.CharField(null=False, max_length=100, unique=True)
-    translations = models.JSONField(null=True)
-
-    def __str__(self):
-        return self.name
-
-
 class IngredientsAllergy(models.Model):
     class Meta:
         db_table = 'IngredientsAllergy'
@@ -40,3 +51,18 @@ class IngredientsAllergy(models.Model):
 
     ingredient_id = models.IntegerField()
     allergy_id = models.IntegerField()
+
+class Dish(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    comment = models.TextField()
+    ingridients = models.JSONField()
+    translations = models.JSONField()
+    same_dish = models.ForeignKey('self', on_delete=models.CASCADE)
+    image = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'dish'
+
+    def str(self):
+        return self.name
