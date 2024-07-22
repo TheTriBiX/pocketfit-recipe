e81936f649774345849886d6941f69662728b25c
Илья Терешенко <ilya_tereshenko04@mail.ru>
2024-07-22 11:08:12 +0300

    Для нормальной работы миграции

--- pocketfit/recipeservice/models.py
+++ pocketfit/recipeservice/models.py
@@ -5,13 +5,13 @@ class Ingredients(models.Model):
     id = models.AutoField(primary_key=True, auto_created=True)
     name = models.CharField(null=False, max_length=100, unique=True)
     translations = models.JSONField(null=True)
-    Squirrels = models.IntegerField()
-    Fats = models.IntegerField()
-    Carbohydrates = models.IntegerField() 
-    PFC = models.IntegerField() # надо брать из рассчета на m(=100г?)
-    comment = models.TextField(max_length=2000)
-    same_ingridient = models.ForeignKey("self", related_name='related_ingridients', on_delete=models.CASCADE, null=True, blank=True)
-    image = models.CharField(max_length=255)
+    Squirrels = models.IntegerField(null=True, blank=True)
+    Fats = models.IntegerField(null=True, blank=True)
+    Carbohydrates = models.IntegerField(null=True, blank=True) 
+    PFC = models.IntegerField(null=True, blank=True) # надо брать из рассчета на m(=100г?)
+    comment = models.TextField(max_length=2000, null=True, blank=True)
+    same_ingridient = models.ForeignKey("self", related_name='related_ingridients', null=True, blank=True, on_delete=models.CASCADE)
+    image = models.CharField(max_length=255, null=True, blank=True)
     class Meta:
         db_table = 'Ingredients'
     def __str__(self):
@@ -26,11 +26,9 @@ class Allergy(models.Model):
 
     id = models.AutoField(primary_key=True, auto_created=True)
     name = models.CharField(max_length=255)
-    translations = models.JSONField()
-    comment = models.TextField()
-    foods = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
-
-
+    translations = models.JSONField(null=True, blank=True)
+    comment = models.TextField(null=True, blank=True)
+    foods = models.ForeignKey(Ingredients, on_delete=models.CASCADE, null=True, blank=True)
     def __str__(self):
         return self.name
 
@@ -49,17 +47,17 @@ class IngredientsAllergy(models.Model):
         db_table = 'IngredientsAllergy'
         unique_together = ('ingredient_id', 'allergy_id')
 
-    ingredient_id = models.IntegerField()
-    allergy_id = models.IntegerField()
+    ingredient_id = models.IntegerField(null=True, blank=True)
+    allergy_id = models.IntegerField(null=True, blank=True)
 
 class Dish(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    comment = models.TextField()
-    ingridients = models.JSONField()
-    translations = models.JSONField()
-    same_dish = models.ForeignKey('self', on_delete=models.CASCADE)
-    image = models.CharField(max_length=255)
+    comment = models.TextField(null=True, blank=True)
+    ingridients = models.JSONField(null=True, blank=True)
+    translations = models.JSONField(null=True, blank=True)
+    same_dish = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    image = models.CharField(max_length=255, null=True, blank=True)
 
     class Meta:
         db_table = 'dish'
